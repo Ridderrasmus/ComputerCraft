4411368e5d602b9a8b967ab750f95caebc588ad5
Rasmus Tanggaard <rasmus@tthost.dk>
2022-10-25 00:42:44 +0200

    Added new vertical digging function

--- Turtle/mine.lua
+++ Turtle/mine.lua
@@ -72,6 +72,41 @@ local function strip(len)
     end
 end
 
+local function digvertical3x3(len)
+    -- Strip mining loop
+    -- TODO: count steps and return to start when done, stuck, or out of fuel
+    -- TODO: check for lava and deal with it
+
+    len = len or 0
+
+    for i = 1, len do
+        digDown()
+        turtle.down()
+        turtle.turnLeft()
+        dig()
+        turtle.forward()
+        turtle.turnRight()
+        dig()
+        turtle.turnLeft()
+        turtle.turnLeft()
+        dig()
+        turtle.turnRight()
+        turtle.back()
+        turtle.turnRight()
+        turtle.turnRight()
+        dig()
+        turtle.forward()
+        turtle.turnRight()
+        dig()
+        turtle.turnLeft()
+        turtle.turnLeft()
+        dig()
+        turtle.turnRight()
+        turtle.back()
+        turtle.turnLeft()
+    end
+end
+
 
 -- Main execution
 while true do
@@ -82,13 +117,14 @@ while true do
         print("0. Terminate program")
         print("1. Stairs")
         print("2. 3x3 Strip tunnel")
+        print("3. 3x3 Vertical tunnel")
         print("Enter the number of the operation you want to perform")
         selection = false
         selection = tonumber(read())
         if (selection == false) or (selection == nil) then
             print("Invalid input, please enter a number")
         else
-            if ((-1 < selection) and (selection < 3)) then
+            if ((-1 < selection) and (selection < 4)) then
                 break
             else 
                 print("No such program number, please enter one of the previously mentioned programs")
@@ -129,6 +165,22 @@ while true do
         end
         strip(length)
         print("Strip mine complete")
+    elseif selection == 3 then
+        while true do
+            term.clear()
+            term.setCursorPos(0,0)
+            print("3x3 downward mine selected.")
+            print("Enter the number of length of tunnel you want to dig")
+            length = false
+            length = tonumber(read())
+            if (length == false) or (length == nil) then
+                print("Invalid input, please enter a number")
+            else
+                break
+            end
+        end
+        digvertical3x3(length)
+        print("Vertical mine complete")
     else 
         print("Invalid selection")
     end
@@ -136,3 +188,4 @@ while true do
     term.clear()
 end
 term.clear()
+term.setCursorPos(0,0)
